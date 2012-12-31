3a6503a841f93667e4035e0fca06d9e230a0549d
unicorim <freddigbjk@gmail.com>
2012-12-31 23:14:56 +1100

    im not using the reg md..hehehe rebel

---
+++ lib/markdown.py
@@ -0,0 +1,24 @@
+import re
+
+def process(string):
+
+    ignore = re.findall(r"(\\\*.*?\\\*)", string)
+    for i in ignore:
+        string = string.replace(i, "|"*15)    
+    string = re.sub(r"\*\*(.*?)\*\*", r"<b>\1</b>", string)
+
+    string = re.sub(r"\*(.*?)\*", r"<i>\1</i>", string)
+    supers = re.findall(r"(?<=\^)(.*?)($|[ ])", string)
+    if supers:
+        replacers = [x[0] for x in supers]
+        for r in replacers:
+            replaced = r.replace("^", "<sup>")
+            replaced += "</sup>"*(len(replacers)-1)
+            string = string.replace(r, replaced)
+    string = re.sub(r"~~(.*?)~~", r"<del>\1</del>", string)
+    string = re.sub(r"\[(.*?)\]\((.*?)\)", r"<a href='\2'>\1</a>", string)
+    string = re.sub(r"`(.*?)`", r"<code>\1</code>", string)
+    string = re.sub(r"(?m)    (.*)(?=($|[\n]))", r"<pre><code>\1</code></pre>", string)
+    for i in ignore:
+        string = string.replace("|"*15, i)
+    return string
